3f7da9d93cfa451f257d4dd88568cf4279cb7c30
Empty <2460752764@qq.com>
2025-04-05 01:08:49 +0800

    4.5: add codes

--- 图论/H. Mad City.cpp
+++ 图论/H. Mad City.cpp
@@ -12,53 +12,41 @@ inline void solve() {
         cout << "NO" << endl;
         return ;
     }
-    bool in = 0; // b在环上
-    int id = 0; // 交汇点
-    vector<bool> vis(n + 1, false);
-    vector<int> dis(n + 1); int disb = 0;
-    function<void(int, int)> dfs1 = [&](int u, int fa) {
-        if(in || id) return ;
+    int id = 0, minn = 1e9; // b到环的最小距离交汇点
+    vector<int> dis(n + 1, -1); // b到其他点的距离
+    function<void(int, int, int)> dfs1 = [&](int u, int fa, int tot) {
+        if(dis[u] == -1) dis[u] = tot;
+        else { // 已经来过
+            if(dis[u] < minn) { // 能更新
+                minn = dis[u];
+                id = u;
+            }
+            return ;
+        }
         for(int v: g[u]) {
             if(v == fa) continue;
-            if(v == b) {
-                in = 1;
-                return ;
-            }
-            if(vis[v]) {
-                id = v;
-                return ;
-            }
-            vis[v] = 1;
-            dis[v] = dis[u] + 1;
-            dfs1(v, u);
-        } 
-    };
-    function<void(int, int)> dfs2 = [&](int u, int fa) {
-        for(int v: g[u]) {
-            if(v == fa) continue;
-            if(v == id) {
-                return ;
-            }
-            dis[v] = dis[u] + 1;
-            dfs2(v, u);
+            dfs1(v, u, tot + 1);
         }
     };
-    vis[b] = 1;
-    dfs1(b, -1);
-    disb = dis[id];
-    if(in) {
-        cout << "YES" << endl;
-        return ;
-    }
-    vis.assign(n + 1, false);
-    dis.assign(n + 1, 0);
-    dfs2(a, -1);
+    dfs1(b, 0, 0);
+    int disb = dis[id];
+    dis.assign(n + 1, -1); // a到其他点的距离
+    function<void(int, int, int)> dfs2 = [&](int u, int fa, int tot) {  
+        if(dis[u] == -1) dis[u] = tot;
+        else {  // 来过
+            if(dis[u] > tot) dis[u] = tot; // 能更新，继续搜
+            else return ; // 不能更新->该路径没潜力，回溯
+        }
+        for(int v: g[u]) {
+            if(v == fa) continue;
+            dfs2(v, u, tot + 1);
+        }
+    };
+    dfs2(a, 0, 0);
     int disa = dis[id];
-    if(disa > disb) {
+    if(disa > disb)
         cout << "YES" << endl;
-        return ;
-    }
-    cout << "NO" << endl;
+    else cout << "NO" << endl;
 }
 int main() {
     int t; cin >> t;
